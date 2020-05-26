fee285bf2bd698ab89bc4bc1c4c67887b76945cf
Keijiro Takahashi <keijiro@gmail.com>
2020-05-26 17:59:44 +0900

    Scene Controller

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.VFX;
+using Unity.Mathematics;
+using Guid = System.Guid;
 using IEnumerator = System.Collections.IEnumerator;
+using Random = Unity.Mathematics.Random;
 
 class SceneController : MonoBehaviour
 {
@@ -8,6 +13,11 @@ class SceneController : MonoBehaviour
 
     [SerializeField] VisualEffect[] _vfxGroup1 = null;
     [SerializeField] VisualEffect[] _vfxGroup2 = null;
+    [SerializeField] float _fadingDuration = 3;
+    [SerializeField] float _vfxDurationMin = 1;
+    [SerializeField] float _vfxDurationMax = 10;
+    [SerializeField] float _hueDuration = 10;
+    [SerializeField] uint _randomSeed = 0;
 
     #endregion
 
@@ -15,7 +25,9 @@ class SceneController : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(VfxCoroutine());
+        PrepareRandom();
+        StartCoroutine(VfxCoroutine(_vfxGroup1));
+        StartCoroutine(VfxCoroutine(_vfxGroup2));
         StartCoroutine(ColorCoroutine());
     }
 
@@ -23,28 +35,48 @@ class SceneController : MonoBehaviour
 
     #region Controller coroutines
 
-    IEnumerator VfxCoroutine()
+    IEnumerator VfxCoroutine(VisualEffect[] vfxGroup)
     {
         while (true)
+            foreach (var vfx in vfxGroup.OrderBy(e => _random.NextUInt()))
+                yield return VfxSingleCoroutine(vfx);
+    }
+
+    IEnumerator VfxSingleCoroutine(VisualEffect vfx)
+    {
+        vfx.enabled = true;
+
+        for (var fade = 0.0f; fade < 1; fade += FaderDelta)
         {
-            var i1 = Random.Range(0, _vfxGroup1.Length);
-            var i2 = Random.Range(0, _vfxGroup2.Length);
-
-            _vfxGroup1[i1].enabled = true;
-            _vfxGroup2[i2].enabled = true;
-
-            yield return new WaitForSeconds(60);
-
-            _vfxGroup1[i1].enabled = false;
-            _vfxGroup2[i2].enabled = false;
+            vfx.SetFloat(IDs.Fader, fade);
+            yield return null;
         }
+
+        vfx.SetFloat(IDs.Fader, 1);
+
+        yield return new WaitForSeconds(RandomVfxDuration);
+
+        for (var fade = 1.0f; fade > 0; fade -= FaderDelta)
+        {
+            vfx.SetFloat(IDs.Fader, fade);
+            yield return null;
+        }
+
+        vfx.SetFloat(IDs.Fader, 0);
+
+        yield return new WaitForSeconds(4);
+
+        vfx.enabled = false;
     }
 
     IEnumerator ColorCoroutine()
     {
+        var hue = 0.0f;
+
         while (true)
         {
-            var hue = Time.time / 120;
+            hue += Time.deltaTime / _hueDuration;
+
             var keyColor = HueToRgb(hue);
             var altColor = HueToRgb(hue + 1.0f / 3);
 
@@ -64,12 +96,29 @@ class SceneController : MonoBehaviour
 
     static class IDs
     {
+        public static int Fader = Shader.PropertyToID("Fader");
         public static int KeyColor = Shader.PropertyToID("KeyColor");
         public static int AltColor = Shader.PropertyToID("AltColor");
     }
 
+    Random _random;
+
+    float FaderDelta
+      => Time.deltaTime / _fadingDuration;
+
+    float RandomVfxDuration
+      => _random.NextFloat(_vfxDurationMin, _vfxDurationMax);
+
     Color HueToRgb(float hue)
       => Color.HSVToRGB(hue % 1, 1, 1);
+
+    void PrepareRandom()
+    {
+        _random = new Random(_randomSeed);
+        _random.NextUInt();
+        _random.NextUInt();
+        _random.NextUInt();
+    }
 
     void SetKeyColors(VisualEffect vfx, Color keyColor, Color altColor)
     {
